6b118da9277008fc4d91c5a937d360a032dd05ad
lionel509 <lionelweng@gmail.com>
2026-04-24 17:43:22 -0400

    feat: restructure project directory and implement motor control and sensor test modules

--- header/game-menu/menu.hpp
+++ header/game-menu/menu.hpp
@@ -1,1 +1,10 @@
-class menu {}
+#pragma once
+
+// Stub for future in-game menu (mode selection, settings, etc.)
+// Intended to be fleshed out once the menu UI is designed.
+class Menu {
+public:
+  void show()   {}
+  void hide()   {}
+  bool isOpen() { return false; }
+};
